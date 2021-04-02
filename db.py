a91b9b4dea89318f6eb8b4228f00f3f87d1ba8fd
isv <isv@B04s-iMac.local>
2021-04-02 10:40:16 +0700

    commit;

--- db.py
+++ db.py
@@ -6,6 +6,7 @@ connect(config.MONGO_DB)
 class User(Document):
     username = StringField(required=True)
     password = StringField(required=True)
+    user_id = StringField(required=True)
 
 class Post(Document):
     pass
--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends, Request
 from pydantic import BaseModel, EmailStr
 from models import user as userModel
+import uuid
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from .. import auth, db
@@ -22,16 +23,29 @@ def get_config():
 def authExceptionHandler(request: Request, exc: AuthJWTException):
     return {"message" : exc.message }
 
+@router.middleware("http")
+async def successfulRequest(request: Request, call_next):
+    response = await call_next(request)
+    return {'message' : 'successful', 'data' : response}
+
 @router.post('/login')
 async def login(user: User):
+    data = db.User.objects(user.username)
     pass
 
 @router.post('/create')
 async def create(user: User):
+    user_id = str(uuid.uuid4())
     db.User(
         username = user.username,
-        password = user.password
+        password = user.password,
+        user_id = user_id
     ).save()
+    return {
+        'user_id' : user_id
+    }
+
+
 
 
     
